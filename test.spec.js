a17eea9fe2d55244cd90f3d2062eda4710b9f148
Linus Styrén <linus.styren@ga.ntig.se>
2020-10-13 00:48:19 +0200

    use fromURL instead and save the day

--- test.spec.js
+++ test.spec.js
@@ -5,7 +5,7 @@ import { TestHelper } from './test-helpers.js'
 import fs from 'fs'
 import path from 'path'
 
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+//const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 
 let h
 let dom
@@ -17,40 +17,20 @@ describe('index.html', () => {
     // to getting the code in the script tag to execute.
     // This is indeed dangerous and should only be done with trusted content.
     // https://github.com/jsdom/jsdom#executing-scripts
-    let dom = new JSDOM(html, { runScripts: "outside-only" });
-    let localStorageHack = `
-    const _localStorage = {
-        getItem: function (key) {
-            return this[key];
-        },
-        setItem: function (key, value) {
-            this[key] = value;
-        }
-    };
-    `;
-    dom.window.eval(`document.head.appendChild(function(){
-        let elm = document.createElement('script');
-        elm.innerHTML = \`${localStorageHack}\`;
-        return elm;
-    }());`);
-    const js = fs.readFileSync(path.resolve(__dirname, html.match(/script.+src="(.+)"/)[1]), 'utf8').replace(/[`.*+?^${}()|[\]\\]/g, '\\$&').replace(/localStorage/g, '_$&');
-    dom.window.eval(`document.body.querySelector('script').removeAttribute('src')`);
-    dom.window.eval(`document.body.querySelector('script').innerHTML = \`${js}\``)
+    let url = 'http://host.docker.internal:8000';
 
     let options = {
-        resources: "usable",
-        runScripts: 'dangerously',
-        cookieJar: new CookieJar(),
-        url: new URL("file:" + path.resolve('./index.html')) 
+        resources: 'usable',
+        runScripts: 'dangerously'
     }
 
-    dom = new JSDOM(dom.serialize(), options);
-
-    setTimeout(() => {
-        container = dom.window.document.body
-        h = new TestHelper(container)
-        done();
-    }, 500);
+    JSDOM.fromURL(url, options).then((dom) => {
+        setTimeout(() => {
+            container = dom.window.document.body
+            h = new TestHelper(container)
+            done();
+        }, 500);
+    })
   })
 
   it('has a main', () => {
